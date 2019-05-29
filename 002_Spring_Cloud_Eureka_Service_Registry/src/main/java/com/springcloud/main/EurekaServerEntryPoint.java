172b59732280aec31298aef6f5e7b36f8152a73c
ManjuPriyaVMP <vmp.cse@gmail.com>
2019-05-29 13:19:59 +0530

    eureka

---
+++ 002_Spring_Cloud_Eureka_Service_Registry/src/main/java/com/springcloud/main/EurekaServerEntryPoint.java
@@ -0,0 +1,12 @@
+package com.springcloud.main;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerEntryPoint {
+public static void main(String args[]) {
+	SpringApplication.run(EurekaServerEntryPoint.class,args);
+}
+}
